ed7eb21306c914ec5eab50b9824a6cc5c47b0b1d
retumaru <220109uj@yse-c.net>
2023-10-04 12:02:28 +0900

    now

--- express_sample/server.js
+++ express_sample/server.js
@@ -1,3 +1,31 @@
 const express = require("express")
 
 const dotenv = require("dotenv")
+
+dotenv.config()
+const HOST = process.env.HOST
+const PORT = process.env.PORT
+
+console.log(HOST)
+console.log(PORT)
+
+//　サーバ作成
+const app = express()
+
+// GETリクエストの処理
+app.get("/", (req, res) => {
+    //　リクエストの処理
+    console.log(req.body)
+    console.log(req.url)
+    console.log(req.query)
+
+    //　レスポンスの処理
+    res.send("Hello!!!!!")
+})
+
+// サーバ待機 (Listen)
+app.listen(PORT, HOST, () => {
+    console.log(HOST)
+    console.log(PORT)
+    console.log("wait...")
+})
