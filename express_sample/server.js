067c93ca7d980a611788dcf2dcf576fc7627689d
retumaru <220109uj@yse-c.net>
2023-10-11 12:01:38 +0900

    update

--- express_sample/server.js
+++ express_sample/server.js
@@ -1,31 +1,32 @@
-const express = require("express")
+// expressモジュール読み込み
+const express = require('express')
+// dotenvモジュール読み込み
+const dotenv = require('dotenv')
+// routerモジュール読み込み
+const routes = require("./routes")
 
-const dotenv = require("dotenv")
-
+// dotenvの設定読み込み
 dotenv.config()
 const HOST = process.env.HOST
 const PORT = process.env.PORT
 
-console.log(HOST)
-console.log(PORT)
-
-//　サーバ作成
+// サーバ作成
 const app = express()
 
-// GETリクエストの処理
-app.get("/", (req, res) => {
-    //　リクエストの処理
-    console.log(req.body)
-    console.log(req.url)
-    console.log(req.query)
+// ミドルウェアの設定
+// publicフォルダを静的コンテンツのフォルダに設定
+app.use(express.static(__dirname + '/public'))
 
-    //　レスポンスの処理
-    res.send("Hello!!!!!")
-})
+// URLエンコード
+app.use(express.urlencoded({ extended: true }))
 
-// サーバ待機 (Listen)
+// ルーティングを有効
+app.use(routes)
+
+//　サーバ停止: 起動中のターミナルで Ctrl + C
+// サーバ待機（Listen）
 app.listen(PORT, HOST, () => {
     console.log(HOST)
     console.log(PORT)
-    console.log("wait...")
+    console.log('wait...')
 })
